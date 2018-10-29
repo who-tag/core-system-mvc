bdfa38d962c8632d001fa816e972924e0bf1ec49
Dennys Henry <daugmaud@gmail.com>
2018-10-29 12:31:04 +0100

    Modified scripts

--- wwwroot/js/patientdashboard-opdclinic-script.js
+++ wwwroot/js/patientdashboard-opdclinic-script.js
@@ -1,11 +1,11 @@
 var NavigatorController;
+var emrMessages = {};
+emrMessages["numericRangeHigh"] = "value should be less than {0}";
+emrMessages["numericRangeLow"] = "value should be more than {0}";
+emrMessages["requiredField"] = "Ensure details have been filled properly";
+emrMessages["numberField"] = "Value not a number";
 
 jq(function() {
     NavigatorController = new KeyboardController();
 
-    var emrMessages = {};
-    emrMessages["numericRangeHigh"] = "value should be less than {0}";
-    emrMessages["numericRangeLow"] = "value should be more than {0}";
-    emrMessages["requiredField"] = "Ensure details have been filled properly";
-    emrMessages["numberField"] = "Value not a number";
 });
